e5b4613dc689bcd69a2fc6f27636bf5c313fac61
Németh Gábor <60961939+nemethgaborradnoti@users.noreply.github.com>
2025-08-06 20:14:07 +0200

    feature: Copy selected tab's folder tree to other tab's folder tree

--- ViewModels/Functional/OptionsViewModel.cs
+++ ViewModels/Functional/OptionsViewModel.cs
@@ -1,12 +1,27 @@
-﻿using FileCraft.Services.Interfaces;
+﻿using FileCraft.Models;
+using FileCraft.Services.Interfaces;
 using FileCraft.Shared.Commands;
+using FileCraft.ViewModels.Shared;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Windows.Input;
 
 namespace FileCraft.ViewModels.Functional
 {
+    public class TabInfo
+    {
+        public string Name { get; set; }
+        public FolderTreeManager FolderTreeManager { get; set; }
+
+        public TabInfo(string name, FolderTreeManager folderTreeManager)
+        {
+            Name = name;
+            FolderTreeManager = folderTreeManager;
+        }
+    }
+
     public class PresetSlotViewModel : BaseViewModel
     {
         public int PresetNumber { get; }
@@ -64,7 +79,8 @@ namespace FileCraft.ViewModels.Functional
     {
         private readonly ISaveService _saveService;
         private readonly IDialogService _dialogService;
-        public string Version => "v1.0.0";
+        private readonly ISharedStateService _sharedStateService;
+        public string Version => "v2.0.0";
 
         public event Action<int, string>? PresetSaveRequested;
         public event Action<int>? PresetLoadRequested;
@@ -76,13 +92,49 @@ namespace FileCraft.ViewModels.Functional
         public ICommand DeletePresetCommand { get; }
         public ICommand DeleteCurrentSaveCommand { get; }
         public ICommand OpenSaveFolderCommand { get; }
+        public ICommand CopyFolderTreeCommand { get; }
 
         public ObservableCollection<PresetSlotViewModel> PresetSlots { get; } = new();
+        public ObservableCollection<TabInfo> AvailableTabs { get; } = new();
+        public ObservableCollection<TabInfo> AvailableDestinationTabs { get; } = new();
 
-        public OptionsViewModel(ISaveService saveService, IDialogService dialogService)
+        private TabInfo? _selectedSourceTab;
+        public TabInfo? SelectedSourceTab
+        {
+            get => _selectedSourceTab;
+            set
+            {
+                if (_selectedSourceTab != value)
+                {
+                    _selectedSourceTab = value;
+                    OnPropertyChanged();
+                    UpdateAvailableDestinationTabs();
+                }
+            }
+        }
+
+        private TabInfo? _selectedDestinationTab;
+        public TabInfo? SelectedDestinationTab
+        {
+            get => _selectedDestinationTab;
+            set
+            {
+                _selectedDestinationTab = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public OptionsViewModel(
+            ISaveService saveService,
+            IDialogService dialogService,
+            ISharedStateService sharedStateService,
+            FileContentExportViewModel fileContentExportVM,
+            TreeGeneratorViewModel treeGeneratorVM,
+            FolderContentExportViewModel folderContentExportVM)
         {
             _saveService = saveService;
             _dialogService = dialogService;
+            _sharedStateService = sharedStateService;
 
             SavePresetCommand = new RelayCommand(
                 execute: slot =>
@@ -109,13 +161,74 @@ namespace FileCraft.ViewModels.Functional
                 _ => CurrentSaveDeleteRequested?.Invoke());
 
             OpenSaveFolderCommand = new RelayCommand(_ => OpenSaveFolder());
-
+            CopyFolderTreeCommand = new RelayCommand(_ => CopyFolderTree(), _ => CanCopyFolderTree());
 
             for (int i = 1; i <= 5; i++)
             {
                 PresetSlots.Add(new PresetSlotViewModel(i));
             }
             CheckForExistingPresets();
+
+            AvailableTabs.Add(new TabInfo("File Content Export", fileContentExportVM.FolderTreeManager));
+            AvailableTabs.Add(new TabInfo("Tree Generator", treeGeneratorVM.FolderTreeManager));
+            AvailableTabs.Add(new TabInfo("Folder Content Export", folderContentExportVM.FolderTreeManager));
+
+            UpdateAvailableDestinationTabs();
+        }
+
+        private void UpdateAvailableDestinationTabs()
+        {
+            var currentDestination = SelectedDestinationTab;
+            AvailableDestinationTabs.Clear();
+
+            var filteredTabs = SelectedSourceTab == null
+                ? AvailableTabs
+                : AvailableTabs.Where(t => t != SelectedSourceTab);
+
+            foreach (var tab in filteredTabs)
+            {
+                AvailableDestinationTabs.Add(tab);
+            }
+
+            if (SelectedDestinationTab != null && SelectedDestinationTab == SelectedSourceTab)
+            {
+                SelectedDestinationTab = null;
+            }
+        }
+
+        private bool CanCopyFolderTree()
+        {
+            return SelectedSourceTab != null &&
+                   SelectedDestinationTab != null &&
+                   SelectedSourceTab != SelectedDestinationTab &&
+                   !string.IsNullOrWhiteSpace(_sharedStateService.SourcePath);
+        }
+
+        private void CopyFolderTree()
+        {
+            if (!CanCopyFolderTree()) return;
+
+            OnStateChanging();
+
+            var sourceManager = SelectedSourceTab!.FolderTreeManager;
+            var destManager = SelectedDestinationTab!.FolderTreeManager;
+
+            int sourceFolderCount = sourceManager.GetSelectedNodeCount();
+            int destFolderCount = destManager.GetSelectedNodeCount();
+
+            string message = $"Are you sure you want to copy {SelectedSourceTab.Name} tab's folder tree ({sourceFolderCount} folders) to {SelectedDestinationTab.Name} tab's folder tree ({destFolderCount} folders)?";
+
+            bool confirmed = _dialogService.ShowConfirmation(
+                title: "Copy Folder Tree",
+                message: message,
+                iconType: DialogIconType.Warning);
+
+            if (confirmed)
+            {
+                var sourceState = sourceManager.GetFolderStates();
+                destManager.LoadTreeForPath(_sharedStateService.SourcePath, sourceState);
+                _dialogService.ShowNotification("Success", "Folder tree copied successfully.", DialogIconType.Success);
+            }
         }
 
         private void OpenSaveFolder()
